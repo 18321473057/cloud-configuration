d16bad5cc92944c4374c8567c4055def0efa10f5
yangchuanshun <ycs19930606>
2019-12-11 16:09:27 +0800

    inti

---
+++ src/main/java/org/line/com/CloudConfigurationApplication.java
@@ -0,0 +1,21 @@
+package org.line.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class CloudConfigurationApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CloudConfigurationApplication.class);
+    }
+}
